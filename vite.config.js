b6b5786bf801f935b8efdbde1f8d7d6ac7d4c190
bogwi <bogwi@rakumail.jp>
2025-04-19 17:26:45 +0900

    Make Vite base path dynamic for subdomain and subpath

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { copyFileSync } from 'fs'
 
+const base = process.env.VITE_BASE_PATH || '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tr909/',
+  base,
   server: {
     port: 16,
     host: true,
